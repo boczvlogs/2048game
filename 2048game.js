7ee6e2b45427e9d1619112a63f4091491eada551
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-10-20 12:43:15 +0800

    Update 2048game.js

--- 2048game.js
+++ 2048game.js
@@ -17,7 +17,7 @@ function GameManager(size, InputManager, Actuator) {
   this.setup(); 
 }
 
-// Restart the game
+// Restart the game again
 GameManager.prototype.restart = function () {
    this.actuator.restart();
    this.setup();

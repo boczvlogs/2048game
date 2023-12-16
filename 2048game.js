d2fe415d669903f03ee9531dbbfc8bd2ae211f9a
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-12-16 10:29:43 +0800

    Update 2048game.js

--- 2048game.js
+++ 2048game.js
@@ -368,7 +368,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   element.textContent = tile.value;
 
   if (tile.previousPosition) {
-    // Make sure that the tile gets rendered in the previous position first
+    // Make sure that the tile gets rendered in the previous position first.
     window.requestAnimationFrame(function () {
       classes[2] = self.positionClass({ x: tile.x, y: tile.y });
       self.applyClasses(element, classes); // Update the position

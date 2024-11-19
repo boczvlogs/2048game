a0d60b89308398533de43315a5d81f309a080cfc
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2024-11-19 08:44:34 +0800

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -17,7 +17,7 @@ public class Game2048 {
 
     private void addRandomTile() {
         // Add a random tile (2 or 4)
-        // (In this example, we'll just add a 2 for simplicity)
+        // (In this example, we'll just add a two for simplicity)
         int row, col; 
         do {
             row = (int) (Math.random() * SIZE);
